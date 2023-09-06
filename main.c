4d093e9fdd23a1ab08b6bd8f97144c9e9bb9e0a0
ivanZadorozhnyy <zadorohhnyiv@gmail.com>
2023-09-06 19:46:27 +0300

    case1,2

---
+++ main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[80];
+    char inp[1000] = "";
+    int i;
+    int t = 1;
+    while (t == 1) {
+        printf("Choose the command:\n");
+        printf("1. Append text\n");
+        printf("2. Start new line\n");
+        printf("3. save as file\n");
+        printf("4. load file\n");
+        printf("0. Exit\n");
+        scanf("%d", &i);
+        while(getchar() != '\n');
+
+        switch(i) {
+            case 1:
+                printf("Enter text to append:\n");
+                fgets(str, sizeof(str), stdin);
+                strcat(inp, str);
+                break;
+            case 2:
+                printf("New line is started\n");
+                strcat(inp, "\n");
+                break;
+            case 3:
+                printf("Enter the file name for saving:");
+                char file [80];
+                fgets(file, sizeof(file), stdin);
+                break;
+            case 4:
+                printf("Enter the file name for loading:");
+                break;
+            case 0:
+                t = 0;
+                break;
+            default:
+                printf("Invalid command\n");
+                break;
+        }
+    }
+    return 0;
+}
+
